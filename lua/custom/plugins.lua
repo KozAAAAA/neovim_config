aad79e553f31c767d61249b8ffe0e372ac2ad830
KozAAAAA <mateusz-koza@outlook.com>
2023-07-31 15:47:07 +0200

    Copilot autosuggestions

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -1,3 +1,5 @@
+local overrides = require("custom.configs.overrides")
+
 local plugins = {
   {
     "jose-elias-alvarez/null-ls.nvim",
@@ -21,6 +23,17 @@ local plugins = {
         "clang-format",
       }
     }
+  },
+  {
+    "zbirenbaum/copilot.lua",
+    -- Lazy load when event occurs. Events are triggered
+    -- as mentioned in:
+    -- https://vi.stackexchange.com/a/4495/20389
+    -- event = "InsertEnter",
+    -- You can also have it load at immediately at
+    -- startup by commenting above and uncommenting below:
+    lazy = false,
+    opts = overrides.copilot,
   },
 }
 return plugins
